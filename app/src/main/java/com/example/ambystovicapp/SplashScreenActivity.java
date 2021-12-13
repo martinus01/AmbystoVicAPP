ae29e3301c6483ab11870ee7fd615e4eacde3fa3
martinus01 <martin.demazure97@orange.fr>
2021-12-13 10:23:17 +0100

    [martin] add SplashScreenActivity

---
+++ app/src/main/java/com/example/ambystovicapp/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.ambystovicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    private final int SPLASH_SCREEN_DELAY = 10000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        // rediriger ver la page main_activity après 1 secondes.
+        Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent= new Intent(getApplicationContext(), VicMainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+    new Handler().postDelayed(runnable,SPLASH_SCREEN_DELAY);
+    }
+
+}
