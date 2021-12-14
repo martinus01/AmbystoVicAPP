63e36af2f322a23f577cd9f796234c23d95f6cd5
martinus01 <martin.demazure97@orange.fr>
2021-12-14 18:22:58 +0100

    [martin][master] Bouton

--- app/src/main/java/com/example/ambystovicapp/Activity_test.java
+++ app/src/main/java/com/example/ambystovicapp/Activity_test.java
@@ -5,26 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class VicMainActivity extends AppCompatActivity {
-
+public class Activity_test extends AppCompatActivity {
     private ImageButton bouton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
 
-        bouton = (ImageButton) findViewById(R.id.ButonTest);
+
+        bouton = (ImageButton) findViewById(R.id.RetourMain);
         bouton.setOnClickListener(new View.OnClickListener(){
             @Override
-                    public void onClick(View v){
-                    Intent intent= new Intent(getApplicationContext(), Activity_test.class);
-                    startActivity(intent);
-                    finish();
+            public void onClick(View v){
+                Intent intent= new Intent(getApplicationContext(), VicMainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
+
+
+
     }
 }
