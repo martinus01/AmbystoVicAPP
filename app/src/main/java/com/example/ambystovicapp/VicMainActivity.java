21f5f0bf0c4247ed7c3901a352d53f2cae4a3b22
martinus01 <martin.demazure97@orange.fr>
2022-01-20 18:22:42 +0100

    [martin][master] Bouton

--- app/src/main/java/com/example/ambystovicapp/VicMainActivity.java
+++ app/src/main/java/com/example/ambystovicapp/VicMainActivity.java
@@ -4,27 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class VicMainActivity extends AppCompatActivity {
 
     private ImageButton bouton;
-
+    private int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
 
         bouton = (ImageButton) findViewById(R.id.ButonTest);
         bouton.setOnClickListener(new View.OnClickListener(){
             @Override
                     public void onClick(View v){
+                    //i=i++;
+                    //R.id.
+
+
                     Intent intent= new Intent(getApplicationContext(), Activity_test.class);
                     startActivity(intent);
                     finish();
             }
         });
+
+
+
+
+
+        //tentative avec le temps
+        /*
+        bouton = (ImageButton) findViewById(R.id.ButonTest);
+        bouton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(getApplicationContext(), Activity_test.class);
+                startActivity(intent);
+                finish();
+            }
+        },20000);
+            }
+        });*/
     }
 }
