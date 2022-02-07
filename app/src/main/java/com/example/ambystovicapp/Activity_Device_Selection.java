7e1da40839956ef7089211377719144f2b1c6800
martinus01 <martin.demazure97@orange.fr>
2022-02-07 13:12:41 +0100

    [martin][graphic] Bouton

--- app/src/main/java/com/example/ambystovicapp/Activity_Device_Selection.java
+++ app/src/main/java/com/example/ambystovicapp/Activity_Device_Selection.java
@@ -1,19 +1,41 @@
 package com.example.ambystovicapp;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class Activity_Device_Selection extends AppCompatActivity {
 
+    private ImageButton bouton;
+    private ImageButton bouton1;
+    private ImageButton bouton2;
+    private ImageButton bouton3;
+    private ImageButton bouton4;
+    private ImageButton bouton5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_selection);
+
+
+        bouton = (ImageButton) findViewById(R.id.button_carte_apple);
+        bouton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent= new Intent(getApplicationContext(), construction_activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
     }
 
     //code permettant l'utilisation de notre bouton
@@ -22,4 +44,5 @@ public class Activity_Device_Selection extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
 }
